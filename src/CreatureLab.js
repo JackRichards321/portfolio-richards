270150bbe684814074a5f0bf7f9d4422f3b7d777
JackRichards321 <sjackrichards88@gmail.com>
2022-03-01 21:05:14 -0500

    career fair

--- src/CreatureLab.js
+++ src/CreatureLab.js
@@ -7,15 +7,15 @@ const CreatureLab = () => {
         <div className="project-creature">
         <div className="creatureDiv">
             <div id="creature1" className="splitDiv">
-                <p className="blurb">This was the project to prepare me for my senior Capstone project. Myself and a team of 8 of my peers created an information campaign, an animated presentation, and a working, interactive, electronic ballot for NYC's switch to ranked choice voting.</p>
+                <p className="blurb">This is my Senior Capstone project. I am currently working with a team of 2 other developers and 5 New Media Design students on a collaborative drawing experience built for Imagine RIT.</p>
                 <img src="/creature-1.png" alt="CreatureLab" className="creatureImage"></img>
             </div>
             <div id="creature2" className="splitDiv">
                 <img src="/creature-2.png" alt="CreatureLab" className="creatureImage"></img>
-                <p className="blurb">I worked as the Tech Lead on my team. My general development responsibilities included creating many of the extra screens for the ballot. I worked closely with our UI designers to make sure we were coding their vision perfectly. Additionally, I oversaw the entire developer team, organized meetings and coordinated with the project manager.</p>
+                <p className="blurb">My general development responsibilities include line smoothing on the drawing app, and the gallery page. The gallery page will be a dynamically loading grid of images from our database</p>
             </div>
             <div id="creature3" className="splitDiv">
-                <p className="blurb">One hurdle I faced during this project was breaking three of the fingers on my left hand. I had to pivot away from the heavy coding and take a more organizational role. In all honesty, I enjoyed doing more managing than just typing. I also took the lead on the styling of the screens. I made sure all fonts, weights, and colors were perfect.</p>
+                <p className="blurb">A roadblock we have faced so far in this project is developing for the touch screen tablets that Imagine RIT goers will use, instead of typical mouse events. We have also had to think about the physical displaying of this project, people will be waiting in line and viewing our gallery on a large screen from far away.</p>
                 <img src="/creature-3.png" alt="CreatureLab" className="creatureImage"></img>
             </div>
             
--- src/SpudClicker.js
+++ src/SpudClicker.js
@@ -7,12 +7,12 @@ const SpudClicker = () => {
         <div className="project-spud">
         <div className="spudDiv">
             <div id="spud1" className="splitDiv">
-                <p className="blurb">This was the project to prepare me for my senior Capstone project. Myself and a team of 8 of my peers created an information campaign, an animated presentation, and a working, interactive, electronic ballot for NYC's switch to ranked choice voting.</p>
+                <p className="blurb">This was my final project for a mobile app development class. I based this app off of the popular web game "Cookie Clicker", but built in XAML Forms for Android.</p>
                 <img src="/spud-1.png" alt="spudClicker" className="spudImage"></img>
             </div>
             <div id="spud2" className="splitDiv">
                 <img src="/spud-2.png" alt="spudClicker" className="spudImageBottom"></img>
-                <p className="blurb">I worked as the Tech Lead on my team. My general development responsibilities included creating many of the extra screens for the ballot. I worked closely with our UI designers to make sure we were coding their vision perfectly. Additionally, I oversaw the entire developer team, organized meetings and coordinated with the project manager.</p>
+                <p className="blurb">This is my first full mobile app, and I built it from scratch. I worked closely with my professor to get it polished, and it is now a fully functioning app where users can purchase upgrades, collect spuds and share their progress to social media.</p>
             </div>
 
             
